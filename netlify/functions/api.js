58f7773d2526a029ff0b967913c4cf2095b37e10
amusictheorist <a_millaresthomson@outlook.com>
2025-02-26 12:46:40 -0500

    attempting netlify express configuration

---
+++ netlify/functions/api.js
@@ -0,0 +1,9 @@
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
+
+const api = express();
+
+const router = Router();
+api.use('/api/', router);
+
+export const handler = serverless(api);
